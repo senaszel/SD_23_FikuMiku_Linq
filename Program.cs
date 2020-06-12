0610393268d0291ef3a74d65e79c6d87cb0e9f84
senaszel <senaszel@gmail.com>
2020-06-12 02:55:39 +0200

    code beautify

--- Program.cs
+++ Program.cs
@@ -14,31 +14,18 @@ namespace SD_23_FIKUMIKU
                 .ConvertAll(x => x.ToString()
                 );
             IEnumerable<(int nbs, string msgs)> initialHundred = nbs.Zip(msgs);
-            Console.WriteLine("\tinitial hundred:\n");
-            initialHundred.Print();
 
             var by3 = initialHundred.ChangeLabelOn(3, "Fiku");
             var linq_by3 = from nb in initialHundred
                            join test_nb in by3
                            on nb.nbs equals test_nb.number
                            select (nb.nbs, test_nb.msg);
-            Console.WriteLine("\tlinq BY 3:\n");
-            linq_by3.Print();
-
-
             var linqBy3_By5 = linq_by3.ChangeLabelOn(5, "Miku");
-            Console.WriteLine("\tlinq BY 5:\n");
-            linqBy3_By5.Print();
-
-
             var linqBy3_By5_By15 = linqBy3_By5.ChangeLabelOn(15, "Fiku Miku");
-            Console.WriteLine("\tlinq BY 15:\n");
             linqBy3_By5_By15.Print();
 
 
             Console.WriteLine("Program ended.");
-
-
         }
     }
 
@@ -60,9 +47,9 @@ namespace SD_23_FIKUMIKU
         }
 
 
-        public static void Print<T>(this IEnumerable<T> subject)
+        public static void Print(this IEnumerable<(int number, string msg)> subject)
         {
-            subject.ToList().ForEach(x => Console.Write(string.Concat(x, " ")));
+            subject.ToList().ForEach(x => Console.WriteLine(string.Concat(x.msg, " ")));
             Console.WriteLine();
         }
 
